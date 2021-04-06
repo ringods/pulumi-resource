9b06da48eff2212b7a76b5d32bf8605adcfbf627
Ringo De Smet <ringo@de-smet.name>
2021-04-06 16:38:46 +0200

    Using the Pulumi config package for JSON decoding
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- pkg/models/out.go
+++ pkg/models/out.go
@@ -1,9 +1,13 @@
 package models
 
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+)
+
 // OutRequest is the struct representing the JSON coming in via stdin on `out` binary
 type OutRequest struct {
-	Source Source                 `json:"source"`
-	Params map[string]interface{} `json:"params"` // absent on initial request
+	Source Source     `json:"source"`
+	Params config.Map `json:"params"`
 }
 
 // OutResponse is the struct representing the JSON going out via stdout on `out` binary
--- pkg/models/out_test.go
+++ pkg/models/out_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,29 +21,55 @@ func TestDecodeOutRequestWithoutParams(t *testing.T) {
 	assert.NotNil(t, request)
 	assert.NotNil(t, request.Params)
 	assert.Empty(t, request.Params)
-	assert.Equal(t, map[string]interface{}{}, request.Params)
+	assert.Equal(t, config.Map{}, request.Params)
 }
 
 func TestDecodeOutRequestWithFlatParamsList(t *testing.T) {
-	jsonRequest := []byte("{ \"source\": " + source + ", \"params\": { \"key1\": \"value1\", \"key2\": \"value2\" }}")
+	jsonRequest := []byte("{ \"source\": " + source + ", \"params\": { \"proj:key1\": \"value1\", \"proj:key2\": \"value2\" }}")
 	request := OutRequest{}
 	if err := json.Unmarshal(jsonRequest, &request); err != nil {
 		assert.Fail(t, "Failed to unmarshal to OutRequest: %s", err)
 	}
+
+	expected := config.Map{}
+	expected.Set(config.MustMakeKey("proj", "key1"), config.NewValue("value1"), false)
+	expected.Set(config.MustMakeKey("proj", "key2"), config.NewValue("value2"), false)
+
 	assert.NotNil(t, request)
 	assert.NotNil(t, request.Params)
 	assert.NotEmpty(t, request.Params)
-	assert.Equal(t, map[string]interface{}{"key1": "value1", "key2": "value2"}, request.Params)
+	assert.Equal(t, expected, request.Params)
 }
 
 func TestDecodeOutRequestWithFlatParamsListMixedTypes(t *testing.T) {
-	jsonRequest := []byte("{ \"source\": " + source + ", \"params\": { \"key1\": \"value1\", \"key2\": 2 }}")
+	jsonRequest := []byte("{ \"source\": " + source + ", \"params\": { \"proj:key1\": \"value1\", \"proj:key2\": 2 }}")
 	request := OutRequest{}
 	if err := json.Unmarshal(jsonRequest, &request); err != nil {
 		assert.Fail(t, "Failed to unmarshal to OutRequest: %s", err)
 	}
+
+	expected := config.Map{}
+	expected.Set(config.MustMakeKey("proj", "key1"), config.NewValue("value1"), false)
+	expected.Set(config.MustMakeKey("proj", "key2"), config.NewObjectValue("2"), false)
+
 	assert.NotNil(t, request)
 	assert.NotNil(t, request.Params)
 	assert.NotEmpty(t, request.Params)
-	assert.Equal(t, map[string]interface{}{"key1": "value1", "key2": 2}, request.Params)
+	assert.Equal(t, expected, request.Params)
+}
+
+func TestDecodeOutRequestWithStructuredConfig(t *testing.T) {
+	jsonRequest := []byte("{ \"source\": " + source + ", \"params\": { \"proj:data\": {\"active\":true, \"nums\": [ 1, 2, 3 ] } }}")
+	request := OutRequest{}
+	if err := json.Unmarshal(jsonRequest, &request); err != nil {
+		assert.Fail(t, "Failed to unmarshal to OutRequest: %s", err)
+	}
+
+	expected := config.Map{}
+	expected.Set(config.MustMakeKey("proj", "data"), config.NewObjectValue("{\"active\":true,\"nums\":[1,2,3]}"), false)
+
+	assert.NotNil(t, request)
+	assert.NotNil(t, request.Params)
+	assert.NotEmpty(t, request.Params)
+	assert.Equal(t, expected, request.Params)
 }
