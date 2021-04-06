6144f00168279d3c38faabce53ea7f749787b2d3
Ringo De Smet <ringo@de-smet.name>
2021-04-06 17:10:51 +0200

    Pass the pipeline params as stack config to Pulumi
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- pkg/out/out.go
+++ pkg/out/out.go
@@ -40,7 +40,7 @@ func (r Runner) deployWithPulumi(req models.OutRequest) (models.OutResponse, err
 		return models.OutResponse{}, errors.Wrap(err, "Failed to create the stack")
 	}
 	// Set the Pulumi stack configuration. These values are usually in file `Pulumi.<stack>.yaml`
-	//stack.SetAllConfig(ctx, configMap)
+	stack.SetAllConfig(ctx, req.GetConfigMap())
 
 	update, err := stack.Up(ctx)
 	if err != nil {
